8277a1a0bf3a0dfafd6d417df74da6823ff47284
Xander.- <zn.xander@gmail.com>
2024-05-28 08:23:57 -0300

    Agregar validacion de numero negativos

--- ejercicio 4/script.js
+++ ejercicio 4/script.js
@@ -1,23 +1,28 @@
 const $ = (id) => document.getElementById(id)
+const formulario = $("formulario")
 
 // Evento submit del formulario
-$("formulario").addEventListener("submit", (e) => {
+formulario.onsubmit = (e) => {
   // Prevenir el comportamiento predeterminado del formulario
   e.preventDefault()
 
   // Obtener los valores del formulario
-  const estatura = parseFloat(document.getElementById("estatura").value)
-  const peso = parseFloat(document.getElementById("peso").value)
+  const estatura = document.getElementById("estatura").value
+  const peso = document.getElementById("peso").value
 
   // Validar los datos
   if (!estatura || !peso) {
-    alert("Por favor, ingrese valores válidos.")
+    alert("Ingresar valores validos")
+    return
+  }
+  if (estatura < 0 || peso < 0) {
+    alert("Los datos deben ser positivos")
     return
   }
 
   // Calcular el IMC
-  const imc = peso / (estatura * estatura)
+  const imc = peso / estatura ** 2
 
   // Mostrar el resultado en una alerta
   alert(`Tu IMC es ${imc.toFixed(2)}`)
-})
+}
